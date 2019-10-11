c96185ec15a307fe1bd198ba1432a7d676b91f4f
Scott Doggett <sdoggett@lcs.on.ca>
2019-10-11 11:21:30 -0400

    Initial Commit

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Doggett, Scott on 2019-10-11.
+//  Copyright © 2019 Doggett, Scott. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
