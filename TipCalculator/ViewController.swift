a683afff0b71a5bc9f240c094b0d7f5baf69d5fa
Scott Doggett <sdoggett@lcs.on.ca>
2019-10-11 15:29:14 -0400

    got buttons linked to controller

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -9,12 +9,52 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //MARK: Properties
+    
+    //Establish Bill Amount
+    @IBOutlet weak var billAmount: UITextField!
+    
+    //Establish Amount of People
+    @IBOutlet weak var amountOfPeople: UITextField!
+    
+    //Total Tip Label
+    @IBOutlet weak var totalTipLable: UILabel!
+    
+    //Tip Per Person Label
+    @IBOutlet weak var tipPerPersonLable: UILabel!
+    
+    //Establish Tip Variable
+    var tipAmount = 0.0
+    
+    //Establish Tip Percent Variable
+    var tipPercent = 0.0
+    
+    //MARK: Methods
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    //Tip Percentage Buttona (chose what percent to apply)
+    
+    
+    @IBAction func button5P(_ sender: Any) {
+        tipPercent = 0.05
+    }
+    
+    @IBAction func button10P(_ sender: Any) {
+        tipPercent = 0.10
+    }
+    
+    @IBAction func button15(_ sender: Any) {
+        tipPercent = 0.15
+    }
+    
+    @IBAction func button20(_ sender: Any) {
+        tipPercent = 0.20
+    }
+    
 }
 
